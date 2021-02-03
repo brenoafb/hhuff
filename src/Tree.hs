60b4eacdacaaf55fe92276c819cbf7c0cc59eec5
brenoafb <breno.afb@gmail.com>
2021-02-03 14:22:35 -0300

    Add tree data structure

---
+++ src/Tree.hs
@@ -0,0 +1,17 @@
+module Tree where
+
+data Tree a n = Leaf n a
+              | Node n [a] (Tree a n) (Tree a n)
+              deriving (Eq, Show)
+
+getTreeWeight :: Tree a n -> n
+getTreeWeight (Leaf w _)   = w
+getTreeWeight (Node w _ _ _) = w
+
+getTreeElements :: Tree a n -> [a]
+getTreeElements (Leaf _ x) = [x]
+getTreeElements (Node _ xs _ _) = xs
+
+merge :: Num n => Tree a n -> Tree a n -> Tree a n
+merge t1 t2 = Node (getTreeWeight t1 + getTreeWeight t2) xs t1 t2
+  where xs = getTreeElements t1 ++ getTreeElements t2
