67166a15c75ace04355d93b5c9685655b6f551a5
brenoafb <breno.afb@gmail.com>
2021-02-03 14:31:09 -0300

    Refine main

--- app/Main.hs
+++ app/Main.hs
@@ -8,15 +8,24 @@ import qualified Data.Map as M
 main :: IO ()
 main = do
   input <- getLine
+  case f input of
+    Nothing -> putStrLn "Error"
+    Just (encoded, decoded) -> do
+      putStrLn "Encoded input"
+      putStrLn $ concatMap show encoded
+      putStrLn "Decoded"
+      putStrLn decoded
+
+
+f :: String -> Maybe ([Bit], String)
+f input = do
   let f = freqs input
-  print $ "Freqs: " ++ show f
-  let tree = buildTree f
-  print $ "Tree: " ++ show tree
-  let Just encodingTable = mkTable tree
-  print $ "encodingTable: " ++ show encodingTable
-  let decodingTable = M.fromList . map (\(x,y) -> (y,x)) $ M.toList encodingTable
-  print $ "decodingTable: " ++ show decodingTable
-  let Just encodedInput = encode input encodingTable
-  print $ "encodedInput: " ++ show encodedInput
-  let Just decodedInput = decode encodedInput decodingTable
-  print $ "decodedInput: " ++ show decodedInput
+      tree = buildTree f
+  encodingTable <- mkTable tree
+  let decodingTable = reverseMap encodingTable
+  encoded <- encode input encodingTable
+  decoded <- decode encoded decodingTable
+  pure (encoded, decoded)
+
+reverseMap :: (Ord a, Ord b) => M.Map a b -> M.Map b a
+reverseMap = M.fromList . map (\(x,y) -> (y,x)) . M.toList
